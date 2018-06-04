267d9fae4a95c80d45510f75e950a21eccfd144a
Roberto <rmacuna@uninorte.edu.co>
2018-06-04 10:36:06 -0500

    Type: Project Structure And Component Design Message: Designed new buttons with new typography (Source Sans). I created the folder components with the specific css and js file that a single component needs. Author: Roberto Mario Acuña Polo

---
+++ public/javascript/crown.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+	
+	$("div.cr.button.pulse, button.cr.button.pulse, a.menu-item.pulse").click(function(e) {
+		e.preventDefault();
+        $('.ripple').remove();
+
+        var posX = $(this).offset().left,
+            posY = $(this).offset().top,
+            btn_width = $(this).width(),
+            btn_height = $(this).height();
+
+        $(this).prepend("<span class='ripple'></span>");
+
+
+        if (btn_width >= btn_height) {
+            btn_height = btn_width;
+        } else {
+            btn_width = btn_height;
+        }
+
+        var x = e.pageX - posX - btn_width / 2;
+        var y = e.pageY - posY - btn_height / 2;
+
+
+        $(".ripple").css({
+            width: btn_width,
+            height: btn_height,
+            top: y + 'px',
+            left: x + 'px'
+        }).addClass("ripple-active");
+    })
+})
