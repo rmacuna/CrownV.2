9fe1676f48b080602f30962d234e28f52b39d758
rmacuna <rmacuna@uninorte.edu.co>
2018-07-14 17:54:33 -0500

    Type: Componet Code In Develop Message: Something is going wrong with the transition between the collapsing state and the active state

--- public/javascript/crmenubar.js
+++ public/javascript/crmenubar.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
-    $("")
     $('.toggle.menu').click(function() {
-       var target = $(this).attr('toggle')
+        var target = $(this).attr('toggle')
         if ($(this).attr('active') == null) {
             if (target == null) {
                 throw new Error('Toggle attr is null or empty.')
             } else if (target.charAt(0) != "#") {
                 throw new Error('Missing #')
             } else {
-                $(target).addClass('cr_sideNav_show_state')
-                $(this).attr('active', '')
+                $(target).removeClass('cr')
+                $(target).addClass('triggering')
+                setTimeout(function() {
+                	$(target).removeClass('triggering')
+                    $(target).addClass('cr_sideNav_show_state')
+                    $(this).attr('active', '')
+                }, 400)
             }
-        }else{
-        	$(target).removeClass('cr_sideNav_show_state')
-        	$(this).removeAttr('active')
+        } else {
+            $(target).removeClass('cr_sideNav_show_state')
+            $(this).removeAttr('active')
         }
-
     })
 })
