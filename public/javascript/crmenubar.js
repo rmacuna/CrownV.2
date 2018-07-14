3c180320929541ee2020bf8e578eacda80cf66c2
rmacuna <rmacuna@uninorte.edu.co>
2018-07-14 17:13:29 -0500

    T Type: JavaScript Message: Menubars works, but still not animated its relevant to do it

--- public/javascript/crmenubar.js
+++ public/javascript/crmenubar.js
@@ -1,11 +1,20 @@
-$(document).ready(function(){
-	$('.toggle.menu').click(function(){
-		var whichTarget = $(this).attr('toogle')
-		if (whichTarget.length == 0) {
-			throw new Error('Toggle attr is null or empty.')
-		}else if (whichTarget.charAt(0) != "#"){
-			throw new Error('Missing #')
-		}else{
-		}
-	})
+$(document).ready(function() {
+    $("")
+    $('.toggle.menu').click(function() {
+       var target = $(this).attr('toggle')
+        if ($(this).attr('active') == null) {
+            if (target == null) {
+                throw new Error('Toggle attr is null or empty.')
+            } else if (target.charAt(0) != "#") {
+                throw new Error('Missing #')
+            } else {
+                $(target).addClass('cr_sideNav_show_state')
+                $(this).attr('active', '')
+            }
+        }else{
+        	$(target).removeClass('cr_sideNav_show_state')
+        	$(this).removeAttr('active')
+        }
+
+    })
 })
